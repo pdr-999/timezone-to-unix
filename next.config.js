605e09231e4e6338b4cf2ec7859d0548fc124e14
Alva A <rpondering@gmail.com>
2023-12-30 18:01:25 +0700

    feat: out -> docs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  distDir: "docs",
 };
 
 module.exports = nextConfig;
--- out/_next/static/UP9dr1xfWpHvXIe8rkNhY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/4ce8b68a486602f1.css","static/chunks/pages/index-12656c96046643fe.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

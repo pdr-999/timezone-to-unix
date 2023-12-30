550292b2450e35db40b6f0ca88f29e99af5418fa
Alva A <rpondering@gmail.com>
2023-12-30 23:18:38 +0700

    feat: add input & output form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider defaultColorScheme="dark">
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
